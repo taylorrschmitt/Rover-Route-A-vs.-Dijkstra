022460030d341e126f41df67dcc626d9bfadd145
Evanlsmillan <evanschroedermil@ufl.edu>
2025-07-31 16:05:23 -0400

    Meow

---
+++ terrain.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Evan Schroeder on 7/31/25.
+//
+
+#include "terrain.h"
+
+void terrain::populateGraph()
---
+++ terrain.h
@@ -0,0 +1,21 @@
+#include<unordered_map>
+#include<utility>
+#include<vector>
+
+using namespace std;
+#ifndef PATHFINDING_PERFORMANCE_ANALYSIS_TERRAIN_H
+#define PATHFINDING_PERFORMANCE_ANALYSIS_TERRAIN_H
+
+
+class terrain {
+    unordered_map<int, vector<int>> adjacencyList;
+    unordered_map<int, pair<int, int>> mapper;
+    unordered_map<pair<int,int>, int> reverseMapper;
+public:
+    terrain();
+    void populateGraph(vector<vector<int>>);
+
+};
+
+
+#endif //PATHFINDING_PERFORMANCE_ANALYSIS_TERRAIN_H
