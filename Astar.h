21bfac0b1ea9ad94ef75d88dbaa39e52f725c9e8
taylorrschmitt <tayloreschmitt@gmail.com>
2025-08-02 19:34:21 -0400

    Astar algorithm

--- Astar.h
+++ Astar.h
@@ -1,18 +1,32 @@
 //
-// Created by taylo on 8/1/2025.
+// Created by taylor on 8/1/2025.
 //
 
 #ifndef ASTAR_H
 #define ASTAR_H
 #include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include "terrain.h"
 using namespace std;
 
+struct node {
+    float f, g, heuristic;
+    int parent;
+    bool visited = false;
+};
+
 class Astar {
-private:
-    priority_queue<int> openList;
-    priority_queue<int, vector<int>, greater<int>> closedList;
-    int g_cost, f_cost;
+    private:
+        unordered_map<int, node> nodeInfo;
+        priority_queue<pair<float, int>, vector<pair<float, int>>, greater<>> openList;
+        unordered_set<int> closedList;
+
+        float predictHeuristic(pair<float, int> currNode, pair<float, int> destNode);
+        vector<int> newPath(int start, int dest, terrain &graph);
+
     public:
-    Astar();
+    //needed functions: pathfinder, hueristic calulator(g), actual cost calculator(f)
+        vector<pair<int,int>> findPath(terrain &graph, pair<int,int> start, pair<int,int> goal);
 };
 #endif //ASTAR_H
