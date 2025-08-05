6b5394b919c6017d6e93b9e4db51693d6fae5941
taylorrschmitt <tayloreschmitt@gmail.com>
2025-08-04 22:13:10 -0400

    beeeeeeeeeeep

--- main.cpp
+++ main.cpp
@@ -6,44 +6,8 @@
 #include "terrain.h"
 #include "Astar.h"
 #include "Dijkstra.h"
-// #include <SFML/Graphics.hpp>
-//#include <SFML/Window.hpp>
 
 using namespace std;
-//
-// sf::Color tileColors(int weight) {
-//     switch (weight) {
-//         case 0: return sf::Color::White; //no obstacle
-//         case 1: return sf::Color(200, 255, 200); //light green light obstacle
-//         case 2: return sf::Color(100, 250, 100);
-//         case 3: return sf::Color::Yellow;
-//         case 4: return sf::Color(255, 165, 0);
-//         case 6: return sf::Color::Red;
-//         default: return sf::Color::Black; //unknown
-//     }
-// }
-//
-// void drawGrid(sf::RenderWindow &window, const vector<vector<int>> &grid, const vector<pair<int, int>> &pathCoords = {}) {
-//     int tileSize = 20;
-//     for (int row = 0; row< grid.size(); row++) {
-//         for (int col = 0; col< grid[0].size(); col++) {
-//             int positionX = col * tileSize;
-//             int positionY = row * tileSize;
-//             sf::RectangleShape tile(sf::Vector2f(tileSize, tileSize));
-//             tile.setPosition(sf::Vector2f(positionX, positionY));
-//             tile.setFillColor(tileColors(grid[row][col]));
-//
-//             //if tile is already part of the path, override the color
-//             // if (find(pathCoords.begin(), pathCoords.end(), make_pair(row, col)) != pathCoords.end()) {
-//             //     tile.setFillColor(sf::Color::Cyan);
-//             // }
-//
-//             tile.setOutlineColor(sf::Color::Black);
-//             tile.setOutlineThickness(1);
-//             window.draw(tile);
-//         }
-//     }
-// }
 
 int main() {
     while(true){
@@ -132,31 +96,5 @@ int main() {
 
     }
 
-
-    //sf::RenderWindow window(sf::VideoMode({static_cast<unsigned>(columns * 20), static_cast<unsigned>(rows * 20)}), "A* Path");
-
-    // sf::Font font;
-    // if (!font.openFromFile("arial.ttf")) {
-    //     std::cerr << "Error loading font\n";
-    // }
-    //
-    // string costStr = "Cost: " + to_string(testAstar.getTotalDistance(AstarPath, graph));
-    // sf::Text costText(font, costStr, 20);
-    // costText.setFillColor(sf::Color::Black);
-    // costText.setPosition(sf::Vector2f(10, rows * 20 + 5));
-
-    // sf::Event event;
-    //
-    // while (window.isOpen()) {
-    //     while (window.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed())
-    //             window.close();
-    //     }
-    //
-    //     window.clear();
-    //     drawGrid(window, grid, AstarPath);
-    //     window.draw(costText);
-    //     window.display();
-    // }
     return 0;
 }
